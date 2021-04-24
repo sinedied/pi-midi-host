fc2284c0e455d4a980be8a2f64bd8ea484e13607
sinedied <noda@free.fr>
2021-04-24 17:27:17 +0200

    docs: add filter docs

--- connectall.rb
+++ connectall.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/ruby
-# Source: https://gist.github.com/chmanie/4f2838f4548d25b9c883f7d6d074f67c
+# Based on: https://gist.github.com/chmanie/4f2838f4548d25b9c883f7d6d074f67c
 
-# Midi out filter
+# Midi in/out filters
+# -------------------
+# Add regular expression patterns to avoid connect a midi in/out for a
+# specific device. For example, to disable midi out for a Korg NTS-1,
+# add /NTS-1/ to the $noMidiOutDevices list.
 $noMidiOutDevices = []
 $noMidiInDevices = []
 
@@ -15,7 +19,7 @@ def matchList(list, s)
   return false
 end
 
-# unconnect everything
+# Unconnect everything
 system "aconnect -x"
 
 t = `aconnect -i -l`
@@ -25,7 +29,7 @@ $device = 0
 t.lines.each do |l|
   match = /client (\d*)\:(?:\s'(.*?)'\s.*?)?/.match(l)
   name = ""
-  # we skip empty lines and the "Through" port
+  # We skip empty lines and the "Through" port
   unless match.nil? || match[1] == '0' || /Through/=~l
     $device = match[1]
     $devices[$device] = []
@@ -41,7 +45,7 @@ $devices.each do |device1, ports1|
   ports1.each do |port1|
     $devices.each do |device2, ports2|
       ports2.each do |port2|
-        # probably not a good idea to connect a port to itself
+        # Probably not a good idea to connect a port to itself
         isSame = (device1 == device2 && port1 == port2)
         noOut = matchList($noMidiOutDevices, $names[device1])
         noIn = matchList($noMidiInDevices, $names[device2])
