0283a31d68d6224dd56305db357894726ef1676c
sinedied <noda@free.fr>
2021-04-24 15:54:46 +0200

    feat: add midi in/out filters

--- connectall.rb
+++ connectall.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/ruby
 # Source: https://gist.github.com/chmanie/4f2838f4548d25b9c883f7d6d074f67c
 
+# Midi out filter
+$noMidiOutDevices = [/NTS-1/]
+$noMidiInDevices = []
+
+def matchList(list, device)
+  $list.each do |pattern|
+    match = pattern.match(device)
+    unless match.nil?
+      return true
+    end
+  end
+  return false
+end
+
 t = `aconnect -i -l`
 $devices = {}
 $device = 0
@@ -22,7 +36,10 @@ $devices.each do |device1, ports1|
     $devices.each do |device2, ports2|
       ports2.each do |port2|
         # probably not a good idea to connect a port to itself
-        unless device1 == device2 && port1 == port2 
+        isSame = (device1 == device2 && port1 == port2)
+        noOut = matchList($noMidiOutDevices, device1)
+        noIn = matchList($noMidiInDevices, device2)
+        unless isSame || noOut || noIn
           system "aconnect #{device1}:#{port1} #{device2}:#{port2}"
         end
       end
